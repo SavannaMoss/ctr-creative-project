895fc3a6cf0f247071c736d5a74aa0fb97515c66
Savanna Moss <60207230+SavannaMoss@users.noreply.github.com>
2022-04-26 13:21:11 -0400

    Data collection

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -7,25 +7,25 @@ export class Globals {
       title: 'White Privilege and Male Privilege',
       author: 'Peggy McIntosh',
       unit: 1,
-      description: '',
-      source: '',
-      tags: ['race', 'racism', 'feminism', 'sexism', 'misogyny', 'privilege', 'gender', 'anti-Black racism']
+      description: 'A chapter in the book <em>On Privilege, Fraudulence, and Teaching as Learning.</em> In this chapter, McIntosh explains how she came to understand privilege systems via her work expanding college curriculum to include women. She reviews several layers of denial that protect and prevent awareness about male privilege and draws parallels, from her own experience, with the denials that protect white privilege. She includes a list of forty-six ordinary and daily examples of white privilege that her African American colleagues do not experience.',
+      source: 'https://www.taylorfrancis.com/chapters/mono/10.4324/9781351133791-3/white-privilege-male-privilege-personal-account-coming-see-correspondences-work-women-studies-1988-1-peggy-mcintosh',
+      tags: ['anti-Black racism', 'feminism', 'gender', 'misogyny', 'privilege', 'race', 'racism', 'sexism']
     },
     {
       title: 'Defining Racism',
       author: 'Beverly Tatum',
       unit: 1,
-      description: '',
-      source: '',
-      tags: ['race', 'racism', 'prejudice', 'anti-Black racism']
+      description: 'A chapter in the book <em>Why Are All the Black Kids Sitting Together in the Cafeteria?</em> Walk into any racially mixed high school and you will see Black, White, and Latino youth clustered in their own groups. Is this self-segregation a problem to address or a coping strategy? Beverly Daniel Tatum, a renowned authority on the psychology of racism, argues that straight talk about our racial identities is essential if we are serious about enabling communication across racial and ethnic divides. These topics have only become more urgent as the national conversation about race is increasingly acrimonious. This fully revised edition is essential reading for anyone seeking to understand the dynamics of race in America.',
+      source: 'https://www.beverlydanieltatum.com/published-works/',
+      tags: ['anti-Black racism', 'law', 'race', 'racism']
     },
     {
       title: 'Modernity, Philosophy, and Race',
-      author: 'Charles Johnson',
+      author: 'Clarence Johnson',
       unit: 1,
-      description: '',
-      source: '',
-      tags: ['race', 'racism', 'philosophy', 'class', 'rationality', 'identity', 'morality', 'anti-Black racism']
+      description: 'A chapter in the book <em>Cornel West and Philosophy (Africana Thought)</em>. After a brief biographical sketch, Johnson leads us through a comprehensive examination of West\'s philosophy from his conceptions of pragmatism, existentialism, Marxism, and Prophetic Christianity to his persuasive writings on black-Jewish relations, affirmative action, and the role of black intellectuals. Special focus is given to West\'s writings on ethics and social justice, and how these inform his entire theoretical framework.',
+      source: 'https://philpapers.org/rec/JOHCW',
+      tags: ['anti-Black racism', 'class', 'identity', 'morality', 'philosophy', 'race', 'racism', 'rationality']
     },
     {
       title: 'Ain\'t I a Woman? (speech)',
@@ -33,55 +33,55 @@ export class Globals {
       unit: 1,
       description: 'At the 1851 Women\'s Rights Convention held in Akron, Ohio, Sojourner Truth delivered what is now recognized as one of the most famous abolitionist and women\'s rights speeches in American history, "Ain\'t I a Woman?" She continued to speak out for the rights of African Americans and women during and after the Civil War.',
       source: 'https://www.nps.gov/articles/sojourner-truth.htm',
-      tags: ['race', 'racism', 'feminism', 'sexism', 'misogyny', 'gender', 'anti-Black racism']
+      tags: ['anti-Black racism', 'feminism', 'gender', 'misogyny', 'race', 'racism', 'sexism']
     },
     {
       title: 'The Souls of Black Folk',
       author: 'W.E.B. Du Bois',
       unit: 1,
-      description: 'The Souls of Black Folk: Essays and Sketches is a 1903 work of American literature by W. E. B. Du Bois. It is a seminal work in the history of sociology and a cornerstone of African-American literature. The book contains several essays on race, some of which had been published earlier in The Atlantic Monthly.',
+      description: '<em>The Souls of Black Folk: Essays and Sketches</em> is a 1903 work of American literature by W. E. B. Du Bois. It is a seminal work in the history of sociology and a cornerstone of African-American literature. The book contains several essays on race, some of which had been published earlier in <em>The Atlantic Monthly</em>.',
       source: 'https://en.wikipedia.org/wiki/The_Souls_of_Black_Folk',
-      tags: ['race', 'racism', 'double-consciousness', 'ontology', 'white gaze', 'identity', 'anti-Black racism']
+      tags: ['anti-Black racism', 'double-consciousness', 'identity', 'ontology', 'race', 'racism', 'white gaze/racializing vision']
     },
     {
       title: 'The Fact of Blackness',
       author: 'Frantz Fanon',
       unit: 1,
-      description: '',
-      source: '',
-      tags: ['race', 'racism', 'double-consciousness', 'rationality', 'identity', 'person vs subperson', 'anti-Black racism']
+      description: '<em>The Fact of Blackness: Frantz Fanon and Visual Representation</em> is a collection of essays that create a far-reaching and original dialogue between cultural theory and visual practice. The rich insights which emerge from this collection explain why Frantz Fanon\'s seminal texts of the 1950s and 60s, <em>Black Skin White Masks</em> and <em>The Wretched of the Earth</em>, have re-emerged at the forefront of postcolonial studies. Ranging between the contemporary politics of location, everyday traumas of social inequality, and the structures and technologies of representation, these dialogues re-affirm the contention of Fanon\'s writings: that narrative, the media, image and symbol lie at the very heart of the practice of politics and social knowledge.',
+      source: 'https://www.google.com/books/edition/The_Fact_of_Blackness/yGUaAAAAYAAJ?hl=en',
+      tags: ['anti-Black racism', 'double-consciousness', 'identity', 'person vs subperson', 'race', 'racism', 'rationality', 'society']
     },
     {
       title: 'The Racial Contract',
       author: 'Charles Mills',
       unit: 2,
-      description: 'The Racial Contract is an essay by the Jamaican-American philosopher Charles W. Mills in which he attempts to show that, although it is conventional to represent the social contract moral and political theories of Thomas Hobbes, John Locke, Jean-Jacques Rousseau, and Immanuel Kant as neutral with respect to race and ethnicity, in actuality, the philosophers understood them to regulate only relations between whites; in relation to non-whites, these philosophers helped to create a "racial contract", which in both formal and informal ways permitted whites to oppress and exploit non-whites and validate their own moral ideals in dealing with non-whites.',
+      description: '<em>The Racial Contract</em> is an essay by the Jamaican-American philosopher Charles W. Mills in which he attempts to show that, although it is conventional to represent the social contract moral and political theories of Thomas Hobbes, John Locke, Jean-Jacques Rousseau, and Immanuel Kant as neutral with respect to race and ethnicity, in actuality, the philosophers understood them to regulate only relations between whites; in relation to non-whites, these philosophers helped to create a "racial contract", which in both formal and informal ways permitted whites to oppress and exploit non-whites and validate their own moral ideals in dealing with non-whites.',
       source: 'https://en.wikipedia.org/wiki/The_Racial_Contract',
-      tags: ['race', 'racism', 'rationality', 'philosophy', 'society', 'justice', 'class', 'ontology', 'white supremacy', 'epistemology', 'morality', 'norming/normalization/naturalization', 'person vs subperson', 'privilege', 'empathy', 'anti-Black racism']
+      tags: ['anti-Black racism', 'class', 'empathy', 'epistemology', 'law', 'morality', 'norming/normalization/naturalization', 'ontology', 'person vs subperson', 'philosophy', 'privilege', 'race', 'racism', 'rationality', 'society', 'white supremacy']
     },
     {
       title: 'Black Bodies, White Gazes',
       author: 'George Yancy',
       unit: 3,
-      description: 'Black Bodies, White Gazes: The Continuing Significance of Race understands Black embodiment within the context of white hegemony within the context of a racist, anti-Black world. George Yancy examines themes such as double consciousness, invisibility, and corporeal malediction that capture the lived reality of Black bodies under tremendous existential duress. He demonstrates that the Black body is a historically lived text on which whites have inscribed their projections which speak equally forcefully to whites\' own self-conceptions.',
+      description: '<em>Black Bodies, White Gazes: The Continuing Significance of Race</em> understands Black embodiment within the context of white hegemony within the context of a racist, anti-Black world. George Yancy examines themes such as double consciousness, invisibility, and corporeal malediction that capture the lived reality of Black bodies under tremendous existential duress. He demonstrates that the Black body is a historically lived text on which whites have inscribed their projections which speak equally forcefully to whites\' own self-conceptions.',
       source: 'https://books.google.com/books/about/Black_Bodies_White_Gazes.html?id=iyoonjqA0Z0C&source=kp_book_description',
-      tags: ['race', 'racism', 'phenomenology', 'ontology', 'philosophy', 'white gaze', 'double-consciousness', 'norming/normalization/naturalization', 'hypervisibility vs invisibility', 'identity', 'docility', 'objectification', 'white supremacy', 'empathy', 'white racist imaginary', 'anti-Black racism']
+      tags: ['anti-Black racism', 'docility', 'double-consciousness', 'empathy', 'hypervisibility vs invisibility', 'identity', 'norming/normalization/naturalization', 'objectification', 'ontology', 'phenomenology', 'philosophy', 'race', 'racism', 'white gaze/racializing vision', 'white racist imaginary', 'white supremacy']
     },
     {
       title: 'Ain\'t I a Woman?',
       author: 'bell hooks',
       unit: 4,
-      description: 'Ain\'t I a Woman? Black Women and Feminism is a 1981 book by bell hooks titled after Sojourner Truth\'s "Ain\'t I a Woman?" speech. hooks examines the effect of racism and sexism on Black women, the civil rights movement, and feminist movements from suffrage to the 1970s.',
+      description: '<em>Ain\'t I a Woman? Black Women and Feminism</em> is a 1981 book by bell hooks titled after Sojourner Truth\'s "Ain\'t I a Woman?" speech. hooks examines the effect of racism and sexism on Black women, the civil rights movement, and feminist movements from suffrage to the 1970s.',
       source: 'https://en.wikipedia.org/wiki/Ain\'t_I_a_Woman%3F_(book)',
-      tags: ['race', 'racism', 'feminism', 'sexism', 'misogyny', 'gender', 'intersectionality', 'objectification', 'sexual exploitation', 'othering', 'patriarchy', 'docility', 'anti-Black racism']
+      tags: ['anti-Black racism', 'docility', 'feminism', 'gender', 'intersectionality', 'misogyny', 'objectification', 'patriarchy', 'race', 'racism', 'sexism']
     },
     {
       title: 'Demarginalizing the Intersection of Race and Sex',
       author: 'Kimberlé Crenshaw',
       unit: 4,
       description: 'In this seminal work, Kimberlé Crenshaw introduces the concept of "intersectionality," the intersection and interconnectedness of identities, such as race, gender, ethnicity, sexuality, etc.',
-      source: '',
-      tags: ['race', 'racism', 'feminism', 'sexism', 'misogyny', 'intersectionality', 'law', 'philosophy', 'gender', 'patriarchy', 'politics', 'identity', 'anti-Black racism']
+      source: 'https://instituteforpr.org/demarginalizing-the-intersection-of-race-and-sex/',
+      tags: ['anti-Black racism', 'feminism', 'gender', 'identity', 'intersectionality', 'law', 'misogyny', 'patriarchy', 'philosophy', 'politics', 'race', 'racism', 'sexism']
     },
     {
       title: 'A Phenomenology of Invisibility',
@@ -89,7 +89,7 @@ export class Globals {
       unit: 4,
       description: 'This book chapter aims to elaborate further on the racialization of the visual field by expanding Yancy’s claims on invisibility beyond black bodies, to consider specifically the invisibility of Asian and Asian American "yellow" bodies.',
       source: 'https://ucdenver.academia.edu/BoramJeong',
-      tags: ['race', 'racism', 'phenomenology', 'anti-Asian racism', 'philosophy', 'politics', 'white racist imaginary', 'morality', 'norming/normalization/naturalization', 'hypervisibility vs invisibility', 'objectification', 'white gaze']
+      tags: ['anti-Asian racism', 'hypervisibility vs invisibility', 'morality', 'norming/normalization/naturalization', 'objectification', 'phenomenology', 'philosophy', 'politics', 'race', 'racism', 'white gaze/racializing vision', 'white racist imaginary']
     },
     {
       title: 'A Phenomenology of Hesitation',
@@ -97,7 +97,7 @@ export class Globals {
       unit: 4,
       description: 'This paper asks how perception becomes racializing and seeks the means for its critical interruption. My aim is not only to understand the recalcitrant and limitative temporal structure of racializing habits of seeing, but also to uncover the possibilities within perception for a critical awareness and destabilization of this structure. Reading Henri Bergson and Maurice Merleau-Ponty in dialogue with Frantz Fanon, Iris Marion Young and race-critical feminism, I locate in hesitation the phenomenological moment where habits of seeing can be internally fractured. Hesitation, I claim, makes visible the exclusionary logic of racializing and objectifying perception, countering its affective closure and opening it to critical transformation.',
       source: 'https://philpapers.org/rec/ALSAPO-3',
-      tags: ['race', 'racism', 'phenomenology', 'philosophy', 'norming/normalization/naturalization', 'white gaze', 'racializing vision', 'double-consciousness', 'patriarchy', 'gender', 'sexism', 'objectification', 'anti-Muslim racism']
+      tags: ['anti-Muslim racism', 'double-consciousness', 'gender', 'norming/normalization/naturalization', 'objectification', 'patriarchy', 'phenomenology', 'philosophy', 'race', 'racism', 'sexism', 'white gaze/racializing vision']
     }
   ];
 
